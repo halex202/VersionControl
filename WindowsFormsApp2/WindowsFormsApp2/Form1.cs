b4277541d205c2a6e6f7dd398e268d01c7ac902c
Alex <Alex@DESKTOP-GCFL517>
2020-09-29 20:34:38 +0200

    5-ös feladat kész

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
+        }
+    }
+}
