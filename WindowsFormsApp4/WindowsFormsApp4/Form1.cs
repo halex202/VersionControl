efc007cbcee84329dcff76fb728240032b8dd8ae
Alex <Alex@DESKTOP-GCFL517>
2020-10-08 17:59:04 +0200

    5-ös feladat kész.

--- WindowsFormsApp4/WindowsFormsApp4/Form1.cs
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp4.Entities;
 
 namespace WindowsFormsApp4
 {
@@ -14,11 +15,26 @@ namespace WindowsFormsApp4
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+            
         }
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
+
+        }
+        
+       
     }
 }
