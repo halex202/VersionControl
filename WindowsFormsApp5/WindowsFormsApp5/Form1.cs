8813e16088b07b4c73160b1864d09d5984499592
Alex <Alex@DESKTOP-GCFL517>
2020-10-18 18:04:25 +0200

    5-ös feladat kész

--- WindowsFormsApp5/WindowsFormsApp5/Form1.cs
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -7,12 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using WindowsFormsApp5.Entities;
 using WindowsFormsApp5.MnbServiceReference;
 
 namespace WindowsFormsApp5
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
@@ -30,6 +33,26 @@ namespace WindowsFormsApp5
             };
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+            
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+               
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
         
     }
