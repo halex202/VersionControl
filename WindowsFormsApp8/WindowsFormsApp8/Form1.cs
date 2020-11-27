ca47a7444c102b5ecd1380c96a9d38afc7c5051c
Alex <Alex@DESKTOP-GCFL517>
2020-11-27 15:50:42 +0100

    2.feladat kész

--- WindowsFormsApp8/WindowsFormsApp8/Form1.cs
+++ WindowsFormsApp8/WindowsFormsApp8/Form1.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace WindowsFormsApp8
 {
+    
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+
+
         public Form1()
         {
             InitializeComponent();
+
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
         }
     }
 }
